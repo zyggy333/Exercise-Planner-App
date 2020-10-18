1100b224ed34f3ea332e760f572189eb811cae85
Kevin Buss <56094748+zyggy333@users.noreply.github.com>
2020-10-18 10:13:13 -0500

    Add files via upload

---
+++ src/main/java/org/launchcode/exerciseplanner/ExercisePlannerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.exerciseplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisePlannerApplication.class, args);
+	}
+
+}
