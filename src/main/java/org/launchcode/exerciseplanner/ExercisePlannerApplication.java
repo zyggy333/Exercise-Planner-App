e05da276d7e20ab0364bf0ce298d98619f8d450d
AustinAYates <omicron_austin@ymail.com>
2020-10-18 10:29:55 -0500

    Testing Git 10/18/2020 -AY

--- src/main/java/org/launchcode/exerciseplanner/ExercisePlannerApplication.java
+++ src/main/java/org/launchcode/exerciseplanner/ExercisePlannerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExercisePlannerApplication {
 
+	//Testing Git 10/18/2020 -AY
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExercisePlannerApplication.class, args);
 	}
